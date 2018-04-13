6c188e8f620f367f0c41d2b7a26a88e33c4a537f
Ido <ungulient@gmail.com>
2018-04-13 18:51:38 +0300

    Implemented basic imputation (mode\mean)

--- DataPreparation.py
+++ DataPreparation.py
@@ -70,74 +70,72 @@ def test_data_preparation(train_x, train_y, test_x, test_y):
 
 
 def handle_outliers(train, validate, test):
-    # TODO implement
-    train_new = None
-    validate_new = None
-    test_new = None
-    return train_new, validate_new, test_new
+    # TODO improve - currently ignore outliers
+    return
 
 
-def handle_na_imputation(train, validate, test):
-    train_new = None
-    validate_new = None
-    test_new = None
-    return train_new, validate_new, test_new
+def handle_imputation(train, validate, test):
+    # TODO improve - currently imputes mode to categorical and mean to numerical
+    object_features = train.keys()[train.dtypes.map(lambda x: x == 'object')]
+
+    for f in train:
+        value = train[f].dropna().mode() if f in object_features else train[f].dropna().mean()
+
+        impute(train, f, value)
+        impute(validate, f, value)
+        impute(test, f, value)
+
+
+def impute(dataframe, f, value):
+    dataframe.loc[dataframe[f].isnull(), f] = value
 
 
 def handle_scaling(train, validate, test):
-    # TODO implement
-    train_new = None
-    validate_new = None
-    test_new = None
-    return train_new, validate_new, test_new
+    return
 
 
 def handle_feature_selection(train, validate, test):
     # TODO implement
-    train_new = None
-    validate_new = None
-    test_new = None
-    return train_new, validate_new, test_new
+    return
 
 
 def identify_and_set_feature_type(train, validate, test):
     # TODO implement. Dafuq this means?
-    train_new = None
-    validate_new = None
-    test_new = None
-    return train_new, validate_new, test_new
+    return
 
 
 def handle_type_modification(train, validate, test):
     # TODO implement. Meaning turning non numeric types to numeric?
-    train_new = None
-    validate_new = None
-    test_new = None
-    return train_new, validate_new, test_new
+    return
 
 
-def save_as_csv(train, validate, test, train_original, validate_original, test_original):
+def save_as_csv_original(train, validate, test):
+    train.to_csv("train_original.csv", index=False)
+    validate.to_csv("validate_original.csv", index=False)
+    test.to_csv("test_original.csv", index=False)
+
+
+def save_as_csv(train, validate, test):
     # TODO ask about filenames expected
     train.to_csv("train.csv", index=False)
     validate.to_csv("validate.csv", index=False)
     test.to_csv("test.csv", index=False)
-    train_original.to_csv("train_original.csv", index=False)
-    validate_original.to_csv("validate_original.csv", index=False)
-    test_original.to_csv("test_original.csv", index=False)
 
 
 def prepare_data():
     df = read_data(online=False)
 
-    train_original, validate_original, test_original = train_validate_test_split(df)
+    train, validate, test = train_validate_test_split(df)
 
-    train, validate, test = handle_outliers(train_original, validate_original, test_original)
-    train, validate, test = handle_na_imputation(train, validate, test)
-    train, validate, test = handle_type_modification(train, validate, test)
-    train, validate, test = handle_scaling(train, validate, test)
-    train, validate, test = handle_feature_selection(train, validate, test)
+    save_as_csv_original(train, validate, test)
 
-    save_as_csv(train, validate, test, train_original, validate_original, test_original)
+    handle_outliers(train, validate, test)
+    handle_imputation(train, validate, test)
+    handle_type_modification(train, validate, test)
+    handle_scaling(train, validate, test)
+    handle_feature_selection(train, validate, test)
+
+    save_as_csv(train, validate, test)
 
 
 if __name__ == '__main__':
