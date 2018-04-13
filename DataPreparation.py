ebd765631535bc2786d2a2c2ca022580ec4fdeed
Ido <ungulient@gmail.com>
2018-04-14 01:22:28 +0300

    Implemented all basic data preparation methods and basic test results

--- DataPreparation.py
+++ DataPreparation.py
@@ -7,8 +7,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_classif
 
 import os
+
+LABEL_COLUMN = 'Vote'
 
 
 def read_data(online):
@@ -24,14 +28,15 @@ def read_data(online):
 def train_validate_test_split(dataframe):
     train_validate, test = train_test_split(dataframe, test_size=0.2)
     train, validate = train_test_split(train_validate, test_size=0.25)
-    return train, validate, test
+    return train.copy(), validate.copy(), test.copy()
 
 
 def rank_data_preparation(train_x, train_y, validate_x, validate_y):
+    #TODO this is buggy atm
     kf = KFold(n_splits=5)
 
-    data_x = pd.concat(train_x, validate_x)
-    data_y = pd.concat(train_y, validate_y)
+    data_x = pd.concat([train_x, validate_x])
+    data_y = pd.concat([train_y, validate_y])
 
     for k, (train_index, test_index) in enumerate(kf.split(data_x)):
         # Random Forest
@@ -73,19 +78,21 @@ def test_data_preparation(train_x, train_y, test_x, test_y):
 
 def handle_outliers(train, validate, test):
     # TODO improve - currently ignore outliers
-    return
+    return train, validate, test
 
 
 def handle_imputation(train, validate, test):
     # TODO improve - currently imputes mode to categorical and mean to numerical
-    object_features = train.keys()[train.dtypes.map(lambda x: x == 'object')]
+    category_features = train.select_dtypes(include='category').columns
 
     for f in train:
-        value = train[f].dropna().mode() if f in object_features else train[f].dropna().mean()
+        value = train[f].dropna().mode() if f in category_features else train[f].dropna().mean()
 
         impute(train, f, value)
         impute(validate, f, value)
         impute(test, f, value)
+
+    return train, validate, test
 
 
 def impute(dataframe, f, value):
@@ -96,26 +103,62 @@ def handle_scaling(train, validate, test):
     # TODO improve - currently uses standard distribution scaler, only using train data.
     # TODO Pay attention to bonus assignment - asks to first use ALL data, then compare to only train
     scaler = StandardScaler()
-    scaler.fit(train)
 
-    train[:] = scaler.transform(train)
-    validate[:] = scaler.transform(validate)
-    test[:] = scaler.transform(test)
+    non_label_features = train.keys()[train.columns.values != LABEL_COLUMN]
+
+    scaler.fit(train[non_label_features])
+
+    train[non_label_features] = scaler.transform(train[non_label_features])
+    validate[non_label_features] = scaler.transform(validate[non_label_features])
+    test[non_label_features] = scaler.transform(test[non_label_features])
+
+    return train, validate, test
 
 
 def handle_feature_selection(train, validate, test):
-    # TODO implement
-    return
+    # TODO improve - currently uses select k best, only using train data.
+    # TODO Pay attention to bonus assignment - asks to first use ALL data, then compare to only train
+    # TODO Pay attention to k=19. Try other k's?
+
+    train_x, train_y = split_label(train)
+
+    univariate_filter = SelectKBest(mutual_info_classif, k=19).fit(train_x, train_y)
+
+    train = transform(univariate_filter, train)
+    validate = transform(univariate_filter, validate)
+    test = transform(univariate_filter, test)
+
+    return train, validate, test
 
 
-def identify_and_set_feature_type(train, validate, test):
-    # TODO implement. Dafuq this means?
-    return
+def split_label(dataframe):
+    return dataframe.drop([LABEL_COLUMN], axis=1), dataframe.Vote
+
+
+def transform(selector, dataframe):
+    return dataframe[
+        (dataframe.drop([LABEL_COLUMN], axis=1).columns[selector.get_support()]).append(pd.Index([LABEL_COLUMN]))
+    ]
+
+
+def identify_and_set_feature_type(dataframe):
+    object_features = dataframe.select_dtypes(include='object').columns
+
+    for f in object_features:
+        dataframe[f] = dataframe[f].astype('category')
 
 
 def handle_type_modification(train, validate, test):
-    # TODO implement. Meaning turning non numeric types to numeric?
-    return
+    # TODO improve? Meaning turning non numeric types to numeric?
+    category_features = train.select_dtypes(include='category').columns.values
+
+    for f in category_features:
+        if f != LABEL_COLUMN:
+            train[f] = train[f].cat.codes
+            validate[f] = validate[f].cat.codes
+            test[f] = test[f].cat.codes
+
+    return train, validate, test
 
 
 def save_as_csv_original(train, validate, test):
@@ -134,21 +177,33 @@ def save_as_csv(train, validate, test):
 def prepare_data():
     df = read_data(online=False)
 
+    identify_and_set_feature_type(df)
+
     train, validate, test = train_validate_test_split(df)
 
     save_as_csv_original(train, validate, test)
 
-    handle_outliers(train, validate, test)
-    handle_imputation(train, validate, test)
-    handle_type_modification(train, validate, test)
-    handle_scaling(train, validate, test)
-    handle_feature_selection(train, validate, test)
+    train, validate, test = handle_outliers(train, validate, test)
+    train, validate, test = handle_imputation(train, validate, test)
+    train, validate, test = handle_type_modification(train, validate, test)
+    train, validate, test = handle_scaling(train, validate, test)
+    train, validate, test = handle_feature_selection(train, validate, test)
 
     save_as_csv(train, validate, test)
 
 
+def test_results():
+    train = pd.read_csv('train.csv', header=0)
+    test = pd.read_csv('test.csv', header=0)
+    train_x, train_y = split_label(train)
+    test_x, test_y = split_label(test)
+    test_data_preparation(train_x, train_y, test_x, test_y)
+
+
 if __name__ == '__main__':
     prepare_data()
+    test_results()
+
 
 
 
